cc3c0598365a07ba7bb483fd858f4bf9bd1d12ee
iliazaidin <ilia.zaid@gmail.com>
2022-05-31 13:51:22 +0300

    mearging

---
+++ controllers/articles.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+const Article = require('../models/articles');
+const NotFoundError = require('../middlewares/notFoundError');
+const Unauthorized = require('../middlewares/unauthorizedError');
+
+const getArticles = async (req, res, next) => {
+  try {
+    const articles = await Article.find({ owner: req.user._id });
+    if (articles.length === 0) {
+      throw new NotFoundError('No articles found on server');
+    } else res.send(articles);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteArticle = async (req, res, next) => {
+  try {
+    const article = await Article.findById(req.params.articleId).select('+owner');
+    if (!article) {
+      throw new NotFoundError('Article ID not found');
+    } else if (req.user._id !== article.owner.toString()) {
+      throw new Unauthorized('Authorization required');
+    } else {
+      await Article.findByIdAndRemove(article._id.toString());
+      res.status(200).send(article);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createArticle = async (req, res, next) => {
+  try {
+    req.body.owner = req.user;
+    const article = await Article.create(req.body);
+    res.status(201).send(article);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getArticles, deleteArticle, createArticle,
+};
