d21301558c8a58c93d67572c215e4b9be0f12c05
iliazaidin <ilia.zaid@gmail.com>
2022-05-31 14:43:30 +0300

    fix: user name

--- models/users.js
+++ models/users.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      default: 'New User',
+      required: true,
       minlength: 2,
       maxlength: 30,
     },
